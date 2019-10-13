7f72a4741b28c8380477be57053766132b058f23
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-10-14 05:14:25 +0530

    style: Add styling to the calculator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './App.scss';
 class App extends React.Component {
   state = {
     expression: '',
@@ -14,6 +14,10 @@ class App extends React.Component {
 
   handleOperatorInput(operator) {
     let { expression } = this.state;
+
+    if (expression.length === 0) {
+      return;
+    }
 
     if (Number.isNaN(expression.charAt(expression.length - 1))) {
       expression = expression.slice(0, -1);
@@ -34,7 +38,10 @@ class App extends React.Component {
   }
 
   setAnswer() {
-    const answer = this.evaluate();
+    let answer = this.evaluate();
+    if (!answer) {
+      answer = 0;
+    }
     this.setState({ expression: '', result: answer, lastOperator: '' });
   }
 
@@ -65,64 +72,64 @@ class App extends React.Component {
 
     return (
       <div>
-        <p>{expression}</p>
-        <p>{result}</p>
-        <div>
-        <div>
-          <div onClick={() => this.handleNumberInput('7')}>
+        <p className="expression">{expression}</p>
+        <p className="result">{result}</p>
+        <div className="row">
+          <div className="button" onClick={() => this.handleNumberInput('7')}>
             7
           </div>
-          <div onClick={() => this.handleNumberInput('8')}>
+          <div className="button" onClick={() => this.handleNumberInput('8')}>
             8
           </div>
-          <div onClick={() => this.handleNumberInput('9')}>
+          <div className="button" onClick={() => this.handleNumberInput('9')}>
             9
           </div>
-          <div onClick={() => this.handleOperatorInput('/')}>
+          <div className="button" onClick={() => this.handleOperatorInput('/')}>
             /
           </div>
         </div>
-          <div onClick={() => this.handleNumberInput('4')}>
+        <div className="row">
+          <div className="button" onClick={() => this.handleNumberInput('4')}>
             4
           </div>
-          <div onClick={() => this.handleNumberInput('5')}>
+          <div className="button" onClick={() => this.handleNumberInput('5')}>
             5
           </div>
-          <div onClick={() => this.handleNumberInput('6')}>
+          <div className="button" onClick={() => this.handleNumberInput('6')}>
             6
           </div>
-          <div onClick={() => this.handleOperatorInput('*')}>
+          <div className="button" onClick={() => this.handleOperatorInput('*')}>
             *
           </div>
         </div>
-        <div>
-          <div onClick={() => this.handleNumberInput('1')}>
+        <div className="row">
+          <div className="button" onClick={() => this.handleNumberInput('1')}>
             1
           </div>
-          <div onClick={() => this.handleNumberInput('2')}>
+          <div className="button" onClick={() => this.handleNumberInput('2')}>
             2
           </div>
-          <div onClick={() => this.handleNumberInput('3')}>
+          <div className="button" onClick={() => this.handleNumberInput('3')}>
             3
           </div>
-          <div onClick={() => this.handleOperatorInput('-')}>
+          <div className="button" onClick={() => this.handleOperatorInput('-')}>
             -
           </div>
         </div>
-        <div>
-          <div onClick={() => this.handleNumberInput('0')}>
+        <div className="row">
+          <div className="triple-button" onClick={() => this.handleNumberInput('0')}>
             0
           </div>
-          <div onClick={() => this.handleOperatorInput('+')}>
+          <div className="button" onClick={() => this.handleOperatorInput('+')}>
             +
           </div>
         </div>
-        <div>
-          <div onClick={() => this.setAnswer()}>
-            =
-          </div>
-          <div onClick={() => this.clearConsole()}>
+        <div className="row">
+          <div className="double-button" onClick={() => this.clearConsole()}>
             Clear
+          </div>
+          <div className="double-button" onClick={() => this.setAnswer()}>
+            =
           </div>
         </div>
       </div>
