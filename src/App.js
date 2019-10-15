3f87b61f06661c5c9d76a53d76a76b08f1b635ed
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-10-15 05:41:37 +0530

    feat: Add live calcultion history using sockets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import io from 'socket.io-client';
 import './App.scss';
 class App extends React.Component {
   state = {
     expression: '',
     result: 0,
+    logs: [],
   };
+
+  componentDidMount() {
+    this.socket = io('http://localhost:8000');
+    this.socket.on('logs', data => this.setState({ logs: data.calculations }));
+  }
 
   handleNumberInput(number) {
     const { expression } = this.state;
@@ -28,6 +35,7 @@ class App extends React.Component {
 
   setAnswer() {
     const answer = this.evaluate();
+    this.socket.emit('new calculation', { expression: this.state.expression, result: answer });
     this.setState({ expression: '', result: answer });
   }
 
@@ -58,77 +66,88 @@ class App extends React.Component {
   }
 
   render() {
-    const { expression, result } = this.state;
+    const { expression, result, logs } = this.state;
 
     return (
-      <div>
-        <p className="expression">{expression}</p>
-        <p className="result">{result}</p>
-        <div className="row">
-          <div className="button" onClick={() => this.handleNumberInput('7')}>
-            7
+      <>
+        <div className="calculator">
+          <p className="expression">{expression}</p>
+          <p className="result">{result}</p>
+          <div className="row">
+            <div className="button" onClick={() => this.handleNumberInput('7')}>
+              7
+            </div>
+            <div className="button" onClick={() => this.handleNumberInput('8')}>
+              8
+            </div>
+            <div className="button" onClick={() => this.handleNumberInput('9')}>
+              9
+            </div>
+            <div className="button" onClick={() => this.handleOperatorInput('/')}>
+              /
+            </div>
           </div>
-          <div className="button" onClick={() => this.handleNumberInput('8')}>
-            8
+          <div className="row">
+            <div className="button" onClick={() => this.handleNumberInput('4')}>
+              4
+            </div>
+            <div className="button" onClick={() => this.handleNumberInput('5')}>
+              5
+            </div>
+            <div className="button" onClick={() => this.handleNumberInput('6')}>
+              6
+            </div>
+            <div className="button" onClick={() => this.handleOperatorInput('*')}>
+              *
+            </div>
           </div>
-          <div className="button" onClick={() => this.handleNumberInput('9')}>
-            9
+          <div className="row">
+            <div className="button" onClick={() => this.handleNumberInput('1')}>
+              1
+            </div>
+            <div className="button" onClick={() => this.handleNumberInput('2')}>
+              2
+            </div>
+            <div className="button" onClick={() => this.handleNumberInput('3')}>
+              3
+            </div>
+            <div className="button" onClick={() => this.handleOperatorInput('-')}>
+              -
+            </div>
           </div>
-          <div className="button" onClick={() => this.handleOperatorInput('/')}>
-            /
+          <div className="row">
+            <div className="button double-button" onClick={() => this.handleNumberInput('0')}>
+              0
+            </div>
+            <div className="button" onClick={() => this.handleNumberInput('.')}>
+              .
+            </div>
+            <div className="button" onClick={() => this.handleOperatorInput('+')}>
+              +
+            </div>
+          </div>
+          <div className="row">
+            <div className="button orange-button" onClick={() => this.clearConsole()}>
+              CLR
+            </div>
+            <div className="button orange-button" onClick={() => this.clearLastCharacter()}>
+              DEL
+            </div>
+            <div className="button double-button orange-button" onClick={() => this.setAnswer()}>
+              =
+            </div>
           </div>
         </div>
-        <div className="row">
-          <div className="button" onClick={() => this.handleNumberInput('4')}>
-            4
-          </div>
-          <div className="button" onClick={() => this.handleNumberInput('5')}>
-            5
-          </div>
-          <div className="button" onClick={() => this.handleNumberInput('6')}>
-            6
-          </div>
-          <div className="button" onClick={() => this.handleOperatorInput('*')}>
-            *
-          </div>
+        <div className="logs">
+          <ol>
+            {logs.map((calculations, index) => (
+              <li key={index}>
+                {calculations.expression} = {calculations.result}
+              </li>
+            ))}
+          </ol>
         </div>
-        <div className="row">
-          <div className="button" onClick={() => this.handleNumberInput('1')}>
-            1
-          </div>
-          <div className="button" onClick={() => this.handleNumberInput('2')}>
-            2
-          </div>
-          <div className="button" onClick={() => this.handleNumberInput('3')}>
-            3
-          </div>
-          <div className="button" onClick={() => this.handleOperatorInput('-')}>
-            -
-          </div>
-        </div>
-        <div className="row">
-          <div className="button double-button" onClick={() => this.handleNumberInput('0')}>
-            0
-          </div>
-          <div className="button" onClick={() => this.handleNumberInput('.')}>
-            .
-          </div>
-          <div className="button" onClick={() => this.handleOperatorInput('+')}>
-            +
-          </div>
-        </div>
-        <div className="row">
-          <div className="button orange-button" onClick={() => this.clearConsole()}>
-            CLR
-          </div>
-          <div className="button orange-button" onClick={() => this.clearLastCharacter()}>
-            DEL
-          </div>
-          <div className="button double-button orange-button" onClick={() => this.setAnswer()}>
-            =
-          </div>
-        </div>
-      </div>
+      </>
     );
   }
 }
